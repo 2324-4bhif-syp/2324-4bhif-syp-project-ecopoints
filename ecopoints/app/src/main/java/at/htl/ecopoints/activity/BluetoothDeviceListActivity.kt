56b5c7ca74d9529a2e4cf5d98b1f45b1e54b71be
Linus-69420 <linusnestler@gmail.com>
2023-11-11 16:55:24 +0100

    feat: added connection functionality to bluetooth device list

--- ecopoints/app/src/main/java/at/htl/ecopoints/activity/BluetoothDeviceListActivity.kt
+++ ecopoints/app/src/main/java/at/htl/ecopoints/activity/BluetoothDeviceListActivity.kt
@@ -3,10 +3,14 @@ package at.htl.ecopoints.activity
 import android.Manifest
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothSocket
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -19,6 +23,12 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.app.ActivityCompat
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.IOException
+import java.util.UUID
 
 class BluetoothDeviceListActivity : ComponentActivity() {
 
@@ -46,6 +56,50 @@ class BluetoothDeviceListActivity : ComponentActivity() {
         }
     }
 
+    private fun connectToDevice(device: BluetoothDevice) {
+        GlobalScope.launch(Dispatchers.IO) {
+            try {
+                Log.d("Connect", "Try to get socket")
+                val socket: BluetoothSocket = device.createRfcommSocketToServiceRecord(
+                    UUID.fromString(device.uuids.get(0).toString())
+                )
+
+                if (ActivityCompat.checkSelfPermission(
+                        this@BluetoothDeviceListActivity,
+                        Manifest.permission.BLUETOOTH_CONNECT
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return@launch
+                }
+
+                Log.d("Connect", "Try to connect")
+                socket.connect()
+
+                if(socket.isConnected) {
+                    Log.d("Bluetooth", "Connected")
+                } else {
+                    Log.e("Bluetooth", "Not Connected")
+                }
+
+                withContext(Dispatchers.Main) {
+
+                }
+            } catch (e: IOException) {
+                Log.e("Connect", "Could not connect to device", e)
+                withContext(Dispatchers.Main) {
+
+                }
+            }
+        }
+    }
+
     @Composable
     fun bluetoothDeviceList(devices: List<BluetoothDevice>) {
         Column(
@@ -64,7 +118,9 @@ class BluetoothDeviceListActivity : ComponentActivity() {
 
             LazyColumn {
                 items(devices) { device ->
-                    bluetoothDeviceItem(device = device)
+                    bluetoothDeviceItem(device = device) {
+                       connectToDevice(device)
+                    }
                     Divider(color = Color.Gray, thickness = 1.dp)
                 }
             }
@@ -72,7 +128,7 @@ class BluetoothDeviceListActivity : ComponentActivity() {
     }
 
     @Composable
-    fun bluetoothDeviceItem(device: BluetoothDevice) {
+    fun bluetoothDeviceItem(device: BluetoothDevice, onItemClick: () -> Unit) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
@@ -96,6 +152,11 @@ class BluetoothDeviceListActivity : ComponentActivity() {
                 } else {
                     Text(text = "Name: ${device.name}")
                     Text(text = "Address: ${device.address}")
+                    //Text(text = "UUID: ${device.uuids.get(0)}")
+                    Text(text = "Bondstate ${device.bondState}")
+                    Button(onClick = onItemClick) {
+                        Text(text = "Connect")
+                    }
                 }
             }
         }

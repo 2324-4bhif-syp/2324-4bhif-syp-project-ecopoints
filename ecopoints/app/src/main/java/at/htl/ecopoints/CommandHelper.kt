480ae7b5225f6ad916af4acc75aae365f77e6898
Linus-69420 <linusnestler@gmail.com>
2023-11-14 13:38:42 +0100

    244bep-17 feat: added custom obd communication and commands

---
+++ ecopoints/app/src/main/java/at/htl/ecopoints/CommandHelper.kt
@@ -0,0 +1,31 @@
+package at.htl.ecopoints
+
+import android.bluetooth.BluetoothSocket
+
+class OBDCommandHelper(private val obdBluetoothInterface: OBDBluetoothInterface) {
+
+    fun resetOBDSys(): String {
+        val command = "ATZ"
+        return obdBluetoothInterface.sendCommand(command)
+    }
+
+    fun echoOff(): String {
+        val command = "ATE0"
+        return obdBluetoothInterface.sendCommand(command)
+    }
+    fun getEngineRPM(): String {
+        val command = "01 0C"
+
+        return obdBluetoothInterface.sendCommand(command)
+    }
+
+    fun getCoolantTemperature(): String {
+        val command = "01 05"
+        return obdBluetoothInterface.sendCommand(command)
+    }
+
+    fun getVehicleSpeed(): String {
+        val command = "01 0D"
+        return obdBluetoothInterface.sendCommand(command)
+    }
+}
---
+++ ecopoints/app/src/main/java/at/htl/ecopoints/OBDBluetoothInterface.kt
@@ -0,0 +1,42 @@
+package at.htl.ecopoints
+
+import android.bluetooth.BluetoothSocket
+import android.util.Log
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
+import java.nio.charset.Charset
+
+class OBDBluetoothInterface(private val bluetoothSocket: BluetoothSocket) {
+
+    private val TAG = "OBDBluetoothInterface"
+    private val inputStream: InputStream = bluetoothSocket.inputStream
+    private val outputStream: OutputStream = bluetoothSocket.outputStream
+
+    fun sendCommand(command: String): String {
+        try {
+            outputStream.write(command.toByteArray(Charset.defaultCharset()))
+            outputStream.write('\r'.toInt()) // Add carriage return
+
+            val buffer = ByteArray(1024)
+            val bytesRead = inputStream.read(buffer)
+            val response = String(buffer, 0, bytesRead, Charset.defaultCharset()).trim()
+
+            Log.d(TAG,"Command: $command, Response: $response")
+
+            return response
+        } catch (e: IOException) {
+            Log.e(TAG,"Error sending/receiving OBD command: ${e.message}")
+            return ""
+        }
+    }
+
+    fun disconnect() {
+        try {
+            bluetoothSocket.close()
+            Log.d(TAG,"Disconnected")
+        } catch (e: IOException) {
+            Log.e(TAG,"Error while disconnecting: ${e.message}")
+        }
+    }
+}

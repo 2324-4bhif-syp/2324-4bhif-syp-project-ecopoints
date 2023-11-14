948b5fb624a2ba8b4f385067ef134ec473cb48e6
Linus-69420 <linusnestler@gmail.com>
2023-11-14 11:57:45 +0100

    fix: connection issues with bluetooth works now

--- ecopoints/app/src/main/java/at/htl/ecopoints/activity/Obd2ReadingActivity.kt
+++ ecopoints/app/src/main/java/at/htl/ecopoints/activity/Obd2ReadingActivity.kt
@@ -7,14 +7,18 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import at.htl.ecopoints.service.Obd2Service
 import at.htl.ecopoints.ui.theme.EcoPointsTheme
-import kotlinx.coroutines.launch
-import java.util.UUID
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 
 class Obd2ReadingActivity : ComponentActivity() {
@@ -49,24 +53,35 @@ class Obd2ReadingActivity : ComponentActivity() {
         }
     }
 
-
     @Composable
     private fun TestRead() {
         Log.d("Obd2ReadingActivity", "$deviceAddress")
         val service = Obd2Service(deviceAddress)
-        val coroutineScope = rememberCoroutineScope()
-        var rpm = "nix"
+        var rpm by remember { mutableStateOf("0") }
+        var buttonClicked by remember { mutableStateOf(false) }
 
-        val getRPMOnClick: () -> Unit = {
-            coroutineScope.launch {
-                rpm = service.getRpm()
-            }
-        }
-
-        Button(onClick = getRPMOnClick) {
+        Button(onClick = { buttonClicked = true }) {
             Text("Get RPM")
         }
 
+        LaunchedEffect(buttonClicked) {
+            if (buttonClicked) {
+                rpm = getRpm(service)
+                buttonClicked = false // Reset the buttonClicked state
+            }
+        }
+
         Text(text = "CurrentRpm ${rpm}")
+    }
+
+    private suspend fun getRpm(service: Obd2Service): String {
+        // Your suspend logic here
+        // For example, you can call a suspend function from your service
+        return withContext(Dispatchers.IO) {
+            // Call your suspend function here
+            // For example:
+            // service.getRpm()
+            service.getEltonApiRPM()
+        }
     }
 }
--- ecopoints/app/src/main/java/at/htl/ecopoints/service/Obd2Service.kt
+++ ecopoints/app/src/main/java/at/htl/ecopoints/service/Obd2Service.kt
@@ -1,45 +1,85 @@
 package at.htl.ecopoints.service
 
-import android.annotation.SuppressLint
 import android.bluetooth.BluetoothSocket
 import android.util.Log
 import com.github.eltonvs.obd.connection.ObdDeviceConnection
-import com.github.eltonvs.obd.command.ObdResponse
 import com.github.eltonvs.obd.command.engine.RPMCommand
 import java.io.IOException
 import java.io.InputStream
 import java.io.OutputStream
-import java.util.UUID
 
-class Obd2Service {
+class Obd2Service(bluetoothDeviceAddress: String) {
 
-    val deviceAdress: String;
-
-    constructor(bluetoothDeviceAddress: String) {
-        deviceAdress = bluetoothDeviceAddress
-    }
+    val deviceAdress: String = bluetoothDeviceAddress
+    var connection: ObdDeviceConnection? = null
+    val TAG = "Obd2Service"
 
     suspend fun establishCommunicationToDevice(): ObdDeviceConnection? {
         try {
             val bluetoothService = BluetoothService()
-            val bluetoothDeviceListService  = BluetoothDeviceListService()
-            bluetoothService.connectDevice(bluetoothDeviceListService.getDeviceByAddress(deviceAdress)!!)
-            val socket: BluetoothSocket? = bluetoothService.getSocket()
+            val bluetoothDeviceListService = BluetoothDeviceListService()
+            bluetoothService.connectDevice(
+                bluetoothDeviceListService.getDeviceByAddress(
+                    deviceAdress
+                )!!
+            )
+            val socket: BluetoothSocket = bluetoothService.getSocket()
 
             if (socket == null) {
-                Log.e("Obd2Service", "Socket is null")
+                Log.e(TAG, "Socket is null")
+                return null
+            } else if (!bluetoothService.connected()) {
+                Log.e(TAG, "Socket is not connected")
                 return null
             }
-            else if (!socket.isConnected) {
-                Log.e("Obd2Service", "Socket is not connected")
-                return null
+            else if(bluetoothService.connected()){
+                Log.d(TAG, "Socket is connected")
             }
 
             val inputStream: InputStream = socket.inputStream
             val outputStream: OutputStream = socket.outputStream
 
             val obdConnection = ObdDeviceConnection(inputStream, outputStream)
-            return obdConnection;
+            Log.d(TAG, "Input and Output Stream created")
+            return obdConnection
+
+        } catch (e: IOException) {
+            Log.e(
+                TAG,
+                "Error while establishing communication to device with Address $deviceAdress"
+            )
+            Log.e(TAG, e.toString())
+        }
+        return null;
+    }
+
+    suspend fun connect() {
+        var tries = 3
+
+        for (i in 1..tries) {
+            Log.d(TAG, "Trying to connect to device $deviceAdress try $i")
+            if (connection != null) {
+                break
+            }
+            connection = establishCommunicationToDevice()
+        }
+    }
+
+    suspend fun getEltonApiRPM(): String {
+        connect()
+        if (connection == null) {
+            return ""
+        }
+
+
+        try {
+            Log.d(TAG, "Trying to get RPM")
+            val response = connection!!.run(RPMCommand(), delayTime = 500L)
+            Log.d(TAG,"RPM RAW: $response.rawResponse")
+            Log.d(TAG, "RPM: ${response.value}")
+            Log.d(TAG, "RPM: ${response.unit}")
+            Log.d(TAG, "RPM: ${response.formattedValue}")
+            return response.value;
 
         } catch (e: IOException) {
             Log.e(
@@ -48,17 +88,6 @@ class Obd2Service {
             )
             Log.e("Obd2Service", e.toString())
         }
-        return null;
-    }
-
-    suspend fun getRpm(): String {
-        val connection = establishCommunicationToDevice()
-
-        if (connection == null) {
-            return ""
-        }
-
-        val response: ObdResponse = connection.run(RPMCommand())
-        return response.value;
+        return ""
     }
 }

dd7c967e3d673918940d086ca90cb449c81b43e3
Armin1503 <arminsalkovic1503@gmail.com>
2023-11-21 12:23:48 +0100

    244bep-14 | fixed entity annotations

--- backend/src/main/java/at/ecopoints/entity/User.java
+++ backend/src/main/java/at/ecopoints/entity/User.java
@@ -1,19 +1,18 @@
 package at.ecopoints.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.transaction.Transactional;
 
 @Entity
+@Table(name = "ECO_USER")
 public class User {
 
-    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String userName;
     private String password;
     private double ecoPoints;
-    
+
     // region Constructors
     public User() { }
 

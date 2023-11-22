975c0f667225b2909581cda12ef9ccfef0ad8edc
oliver-420 <o.daxinger@students.htl-leonding.ac.at>
2023-11-22 11:14:58 +0100

    244bep-31 feat: added TopBar to Map, got the exact location

--- ecopoints/app/src/main/java/at/htl/ecopoints/activity/MapActivity.kt
+++ ecopoints/app/src/main/java/at/htl/ecopoints/activity/MapActivity.kt
@@ -1,5 +1,7 @@
 package at.htl.ecopoints.activity
 
+import android.annotation.SuppressLint
+import android.icu.lang.UCharacter.VerticalOrientation
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -14,6 +16,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material.*
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,7 +24,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import at.htl.ecopoints.interfaces.OnLocationChangedListener
 import at.htl.ecopoints.service.TestLocationService
@@ -43,6 +49,7 @@ class MapActivity : ComponentActivity(), OnLocationChangedListener {
     private var longitude = 1.35
     private var latitude = 103.87
 
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -55,7 +62,7 @@ class MapActivity : ComponentActivity(), OnLocationChangedListener {
                 position = CameraPosition.fromLatLngZoom(currentLocation, 10f)
             }
             var mapProperties by remember {
-                mutableStateOf(MapProperties(mapType = MapType.NORMAL))
+                mutableStateOf(MapProperties(mapType = MapType.NORMAL, isMyLocationEnabled = true))
             }
             EcoPointsTheme {
                 // A surface container using the 'background' color from the theme
@@ -63,22 +70,27 @@ class MapActivity : ComponentActivity(), OnLocationChangedListener {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GoogleMap(
-                        modifier = Modifier.fillMaxSize(),
-                        cameraPositionState = cameraPositionState,
-                        properties = mapProperties
-                    ) {
-                        Marker(
-                            state = MarkerState(position = currentLocation),
-                            title = "Htl Leonding",
-                            snippet = "Marker in Leonding"
-                        )
-                    }
-                    Column {
-                        MapTypeControls(onMapTypeClick = {
-                            Log.d("GoogleMap", "Selected map type $it")
-                            mapProperties = mapProperties.copy(mapType = it)
+                    Scaffold (topBar = {
+                        TopAppBar(backgroundColor = Color.White, title = {
+                            Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.End) {
+                                MapTypeControls(onMapTypeClick = {
+                                    Log.d("GoogleMap", "Selected map type $it")
+                                    mapProperties = mapProperties.copy(mapType = it)
+                                })
+                            }
                         })
+                    })  {
+                        GoogleMap(
+                            modifier = Modifier.fillMaxSize(),
+                            cameraPositionState = cameraPositionState,
+                            properties = mapProperties
+                        ) {
+                            Marker(
+                                state = MarkerState(position = currentLocation),
+                                title = "Htl Leonding",
+                                snippet = "Marker in Leonding"
+                            )
+                        }
                     }
                 }
             }

a1dcb471310f50c0689593cbf2b9c5f962d7b333
ab321 <abdubed2@gmail.com>
2023-12-02 10:51:26 +0100

    244bep-39 | Created Profile xml-file and linkes it in ProfileActivity

--- ecopoints/app/src/main/java/at/htl/ecopoints/activity/ui/theme/ProfileActivity.kt
+++ ecopoints/app/src/main/java/at/htl/ecopoints/activity/ui/theme/ProfileActivity.kt
@@ -1,6 +1,9 @@
 package at.htl.ecopoints.activity.ui.theme
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ScrollView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
@@ -11,52 +14,22 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import at.htl.ecopoints.R
 import at.htl.ecopoints.activity.ui.theme.ui.theme.EcoPointsTheme
 import at.htl.ecopoints.navigation.BottomNavBar
+import com.google.android.material.composethemeadapter.MdcTheme
+import androidx.compose.ui.viewinterop.AndroidView
 
 class ProfileActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            val (currentScreen, setCurrentScreen) = remember { mutableStateOf("Profile") }
-            at.htl.ecopoints.activity.ui.theme.EcoPointsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Profile")
-                    Box {
-
-                        BottomNavBar(
-                            currentScreen = currentScreen,
-                            onScreenSelected = { newScreen -> setCurrentScreen(newScreen) },
-                            context = this@ProfileActivity
-                        )
-                    }
-                }
-            }
-        }
-
         setContentView(R.layout.activity_profile)
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EcoPointsTheme {
-        Greeting("Profile")
     }
+
 }

96bfae3f0b2ffaf56dd3586a85e3e9995e09b99b
Linus-69420 <Linusnestler@gmail.com>
2023-12-11 19:51:40 +0100

    244bep-26 feat: implementing obd2 api library

--- ecopoints/app/src/main/java/at/htl/ecopoints/activity/TripActivity.kt
+++ ecopoints/app/src/main/java/at/htl/ecopoints/activity/TripActivity.kt
@@ -4,16 +4,15 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothSocket
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ScrollState
-import androidx.compose.foundation.background
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -21,7 +20,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -29,13 +27,13 @@ import androidx.compose.material.Scaffold
 import androidx.compose.material.TopAppBar
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
@@ -55,9 +53,9 @@ import at.htl.ecopoints.service.BluetoothService
 import at.htl.ecopoints.service.Obd2Service
 import at.htl.ecopoints.service.TestLocationService
 import at.htl.ecopoints.ui.theme.EcoPointsTheme
-import com.github.pires.obd.commands.SpeedCommand
-import com.github.pires.obd.commands.temperature.TemperatureCommand
-import com.github.pires.obd.commands.engine.RPMCommand
+import com.github.eltonvs.obd.command.engine.RPMCommand
+import com.github.eltonvs.obd.command.engine.SpeedCommand
+import com.github.eltonvs.obd.connection.ObdDeviceConnection
 import com.github.pires.obd.commands.protocol.EchoOffCommand
 import com.github.pires.obd.commands.protocol.LineFeedOffCommand
 import com.github.pires.obd.commands.protocol.SelectProtocolCommand
@@ -74,7 +72,14 @@ import com.google.maps.android.compose.rememberCameraPositionState
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
 import java.lang.Error
+import java.util.Timer
+import java.util.TimerTask
+import java.util.UUID
 import java.util.concurrent.atomic.AtomicInteger
 import kotlin.math.atan2
 import kotlin.math.cos
@@ -82,6 +87,7 @@ import kotlin.math.sin
 import kotlin.math.sqrt
 
 class TripActivity : ComponentActivity(), OnLocationChangedListener {
+    private val TAG = "TripActivity"
     private var selectedDevice: BluetoothDevice? = null
     private val bluetoothDeviceService = BluetoothDeviceListService()
     private val bluetoothService: BluetoothService = BluetoothService()
@@ -105,7 +111,7 @@ class TripActivity : ComponentActivity(), OnLocationChangedListener {
             var showDialog: Boolean by remember { mutableStateOf(false) }
             var deviceNameText by remember { mutableStateOf("Not Selected") }
             var isConnecting by remember { mutableStateOf(false) }
-            var connection by remember { mutableStateOf(false) }
+            var connection by remember { mutableStateOf("Not Connected") }
             val currentLocation = LatLng(latitude, longitude)
             var cameraPositionState = rememberCameraPositionState {
                 position = CameraPosition.fromLatLngZoom(currentLocation, 10f)
@@ -148,11 +154,10 @@ class TripActivity : ComponentActivity(), OnLocationChangedListener {
                     }
 
                     if (isConnecting) {
-                        ConnectToDevice(connecting = isConnecting,
+                        Connect(connecting = isConnecting,
                             selectedDevice,
                             onDismiss = { isConnecting = false },
                             onConnect = { it ->
-                                isConnecting = it
                                 connection = it
                             })
                     }
@@ -163,7 +168,7 @@ class TripActivity : ComponentActivity(), OnLocationChangedListener {
                         verticalArrangement = Arrangement.Bottom,
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
-                        TestReadCustomComm()
+                        //TestReadCustomComm()
                         Spacer(modifier = Modifier.height(16.dp)) // Space between text and buttons
 
                         StartStopButton()
@@ -280,20 +285,20 @@ class TripActivity : ComponentActivity(), OnLocationChangedListener {
 //                            }
 //                            i.incrementAndGet()
                                         try {
-                                            var socket = service.getSocketTest()
-                                            var spd = SpeedCommand();
-                                            spd.run(socket.inputStream, socket.outputStream);
-
-                                            var r = RPMCommand();
-                                            r.run(socket.inputStream, socket.outputStream);
-                                            var res = r.result;
-                                            Log.d("Test", res)
-                                            res.replace("410C", "").toInt()
-                                            Log.d("Test", res)
-                                            val combinedDecimal = res.toInt(16)
-                                            Log.d("Test", combinedDecimal.toString())
-                                            rpm = (combinedDecimal / 4).toString();
-                                            Log.d("Test", rpm)
+//                                            var socket = service.getSocketTest()
+//                                            var spd = SpeedCommand();
+//                                            spd.run(socket.inputStream, socket.outputStream);
+//
+//                                            var r = RPMCommand();
+//                                            r.run(socket.inputStream, socket.outputStream);
+//                                            var res = r.result;
+//                                            Log.d("Test", res)
+//                                            res.replace("410C", "").toInt()
+//                                            Log.d("Test", res)
+//                                            val combinedDecimal = res.toInt(16)
+//                                            Log.d("Test", combinedDecimal.toString())
+//                                            rpm = (combinedDecimal / 4).toString();
+//                                            Log.d("Test", rpm)
 
 
                                             var t = EngineCoolantTemperatureCommand();
@@ -319,6 +324,168 @@ class TripActivity : ComponentActivity(), OnLocationChangedListener {
         }
     }
 
+    @SuppressLint("MissingPermission", "CoroutineCreationDuringComposition")
+    @Composable
+    private fun Connect(
+        connecting: Boolean,
+        device: BluetoothDevice?,
+        onDismiss: () -> Unit,
+        onConnect: (String) -> Unit
+    ) {
+        if (device == null) {
+            NoDeviceSelectedAlert(onDismiss = onDismiss)
+        } else {
+            val uuid: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
+
+            var inputStream: InputStream? by remember { mutableStateOf(null) }
+            var outputStream: OutputStream? by remember { mutableStateOf(null) }
+            var bluetoothSocket: BluetoothSocket? by remember { mutableStateOf(null) }
+            var isConnected by remember {
+                mutableStateOf(false);
+            }
+
+            LaunchedEffect(Unit) {
+                val job = CoroutineScope(Dispatchers.IO).launch {
+                    try {
+                        onConnect("Connecting ...")
+                        bluetoothSocket = device.createRfcommSocketToServiceRecord(uuid)
+                        bluetoothSocket?.connect()
+                        inputStream = bluetoothSocket?.inputStream
+                        outputStream = bluetoothSocket?.outputStream
+                        onConnect("Connected")
+                        Log.d(TAG, inputStream.toString())
+                        Log.d(TAG, outputStream.toString())
+                        isConnected = true;
+                    } catch (e: IOException) {
+                        Log.e(TAG, e.toString())
+                        onConnect("Failed to connect")
+                    }
+                }
+            }
+
+            if (isConnected) {
+                Read(
+                    bluetoothSocket = bluetoothSocket,
+                    inputStream = inputStream,
+                    outputStream = outputStream
+                )
+            }
+        }
+    }
+
+    @Composable
+    fun Read(
+        bluetoothSocket: BluetoothSocket?, inputStream: InputStream?, outputStream: OutputStream?
+    ) {
+        var rpm by remember { mutableStateOf("0") }
+        var speed by remember { mutableStateOf("0") }
+        var coolantTemp by remember { mutableStateOf("0") }
+        val coroutineScope = rememberCoroutineScope()
+        if (bluetoothSocket != null && inputStream != null && outputStream != null) {
+            EchoOffCommand().run(inputStream, outputStream)
+            LineFeedOffCommand().run(inputStream, outputStream)
+            TimeoutCommand(125).run(inputStream, outputStream)
+            SelectProtocolCommand(ObdProtocols.AUTO).run(inputStream, outputStream)
+
+            val timer = Timer()
+            val task = object : TimerTask() {
+                val obdConnection = ObdDeviceConnection(inputStream, outputStream)
+                suspend fun getRpm(): String {
+                    try {
+                        val res = obdConnection.run(RPMCommand()).value
+                        Log.d(TAG, "RPM: $res")
+                        return res
+
+                    } catch (_: Exception) {
+
+                    }
+                    return "00";
+                }
+
+                suspend fun getSpeed(): String {
+                    try {
+                        val res = obdConnection.run(SpeedCommand()).value
+                        Log.d(TAG, "Speed: $res")
+                        return res
+
+                    } catch (_: Exception) {
+
+                    }
+                    return "00";
+                }
+
+                override fun run() {
+                    try {
+                        coroutineScope.launch(Dispatchers.IO) {
+                            rpm = getRpm()
+                           // speed = getSpeed()
+                        }
+//                        val speedCommand = SpeedCommand();
+//                        val rpmCommand = RPMCommand();
+//                        val coolantTemperatureCommand = EngineCoolantTemperatureCommand()
+//
+////                        speedCommand.run(inputStream, outputStream)
+////                        val speedResponse = speedCommand.result.replace("410D", "")
+////                        speed = speedResponse.toInt(16).toString()
+//
+//                        rpmCommand.run(inputStream, outputStream)
+//                        val rpmResponse = rpmCommand.result.replace("410C", "")
+
+
+//                        coolantTemperatureCommand.run(inputStream, outputStream)
+//                        val coolantResponse = rpmCommand.result.replace("410D", "")
+//                        coolantTemp = (coolantResponse.toInt(16) - 40).toString()
+
+
+//                        val spd = SpeedCommand();
+//                        spd.run(
+//                            inputStream, outputStream
+//                        );
+//                        val speedtes = spd.result;
+//                        Log.d(TAG, "Speed: $speedtes")
+//                        speed = speedtes;
+//
+//                        var r = RPMCommand();
+//                        r.run(
+//                            inputStream, outputStream
+//                        );
+//                        var rpmres = r.result;
+//                        Log.d(TAG, "RPM: $rpmres")
+//                        rpm = rpmres;
+//
+//
+//                        var t = EngineCoolantTemperatureCommand();
+//                        t.run(
+//                            inputStream, outputStream
+//                        );
+//                        coolantTemp = t.result;
+//                        Log.d(TAG, "Cooland: $coolantTemp")
+                    } catch (e: Exception) {
+                        Log.e(TAG, e.toString())
+                    }
+                }
+            }
+
+            timer.schedule(task, 1000, 1000)
+            Thread.setDefaultUncaughtExceptionHandler { _, throwable ->
+                // Handle the uncaught exception here
+                Log.e("UncaughtException", "Unhandled exception: $throwable")
+            }
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(text = "Current-Speed $speed")
+            Text(text = "Current-Rpm $rpm")
+            Text(text = "Coolant-Temp $coolantTemp")
+        }
+    }
+
+
     @Composable
     fun StartStopButton() {
         Row(
@@ -343,13 +510,11 @@ class TripActivity : ComponentActivity(), OnLocationChangedListener {
     }
 
     @Composable
-    fun ConnectionInfo(deviceName: String, connection: Boolean = false) {
-        var connectionState = "Not Connected"
+    fun ConnectionInfo(deviceName: String, connection: String) {
         var connectionStateColor: Color = Color.Red
-        if (connection) {
-            connectionState = "Connected"
-            connectionStateColor = Color.Green
-        }
+        if (connection == "Connected") connectionStateColor = Color.Green
+        else if (connection == "Connecting ...") connectionStateColor = Color.Yellow
+
 
         Row(
             modifier = Modifier.fillMaxWidth(),
@@ -376,7 +541,7 @@ class TripActivity : ComponentActivity(), OnLocationChangedListener {
                 modifier = Modifier.weight(0.4f)
             ) {
                 Text(
-                    text = connectionState,
+                    text = connection,
                     modifier = Modifier.padding(8.dp),
                     fontSize = 12.sp,
                     color = connectionStateColor
@@ -404,7 +569,9 @@ class TripActivity : ComponentActivity(), OnLocationChangedListener {
                 title = { Text("Select a Bluetooth Device") },
                 text = {
                     LazyColumn {
-                        items(pairedDevices) { device ->
+                        items(
+                            pairedDevices.filter { it -> it.name.lowercase().contains("obd") }
+                        ) { device ->
                             TextButton(
                                 onClick = { onDeviceSelected(device) },
                                 modifier = Modifier.fillMaxWidth()
@@ -422,35 +589,35 @@ class TripActivity : ComponentActivity(), OnLocationChangedListener {
         }
     }
 
-    @SuppressLint("CoroutineCreationDuringComposition")
-    @Composable
-    fun ConnectToDevice(
-        connecting: Boolean,
-        device: BluetoothDevice?,
-        onDismiss: () -> Unit,
-        onConnect: (Boolean) -> Unit
-    ) {
-        if (device == null) {
-            NoDeviceSelectedAlert(onDismiss = onDismiss)
-        } else if (connecting) {
-
-            CoroutineScope(Dispatchers.Main).launch {
-                bluetoothService.connectDevice(device)
-                onConnect(bluetoothService.connected())
-            }
-
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color(0x88000000)),
-                contentAlignment = Alignment.Center
-            ) {
-                CircularProgressIndicator(
-                    modifier = Modifier.size(50.dp)
-                )
-            }
-        }
-    }
+//    @SuppressLint("CoroutineCreationDuringComposition")
+//    @Composable
+//    fun ConnectToDevice(
+//        connecting: Boolean,
+//        device: BluetoothDevice?,
+//        onDismiss: () -> Unit,
+//        onConnect: (Boolean) -> Unit
+//    ) {
+//        if (device == null) {
+//            NoDeviceSelectedAlert(onDismiss = onDismiss)
+//        } else if (connecting) {
+//
+//            CoroutineScope(Dispatchers.Main).launch {
+//                bluetoothService.connectDevice(device)
+//                onConnect(bluetoothService.connected())
+//            }
+//
+//            Box(
+//                modifier = Modifier
+//                    .fillMaxSize()
+//                    .background(Color(0x88000000)),
+//                contentAlignment = Alignment.Center
+//            ) {
+//                CircularProgressIndicator(
+//                    modifier = Modifier.size(50.dp)
+//                )
+//            }
+//        }
+//    }
 
     @Composable
     fun NoDeviceSelectedAlert(onDismiss: () -> Unit) {
@@ -463,6 +630,8 @@ class TripActivity : ComponentActivity(), OnLocationChangedListener {
                 }
             })
     }
+
+//region Map
 
     @Composable
     private fun DrawPolyline() {
@@ -557,4 +726,6 @@ class TripActivity : ComponentActivity(), OnLocationChangedListener {
             addItemToList(LatLng(latitude, longitude))
         }
     }
+
+//endregion
 }

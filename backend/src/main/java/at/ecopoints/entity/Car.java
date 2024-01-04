68481a263dda99268fe376b2a9aab3bc81648a9b
Armin1503 <arminsalkovic1503@gmail.com>
2024-01-04 17:49:06 +0100

    244bep-43 | created entity, repository and resource for Car

--- backend/src/main/java/at/ecopoints/entity/Car.java
+++ backend/src/main/java/at/ecopoints/entity/Car.java
@@ -19,4 +19,43 @@ public class Car {
 
     private String model;
 
+    @ManyToOne(cascade = {
+            CascadeType.PERSIST,
+            CascadeType.MERGE,
+            CascadeType.REFRESH,
+            CascadeType.DETACH,
+            CascadeType.REMOVE
+    })
+    private Brand brand;
+
+    //region Constructors
+    public Car(){}
+
+    public Car(String model, Brand brand) {
+        this.model = model;
+        this.brand = brand;
+    }
+    //endregion
+
+    //region Getter and Setter
+    public Long getId() {
+        return id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public Brand getBrand() {
+        return brand;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setBrand(Brand brand) {
+        this.brand = brand;
+    }
+    //endregion
 }

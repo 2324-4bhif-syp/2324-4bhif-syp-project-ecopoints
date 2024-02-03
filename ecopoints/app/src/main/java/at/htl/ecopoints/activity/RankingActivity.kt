2e389b7456277e81f3a856d58f25a5fa7e282463
Armin1503 <arminsalkovic1503@gmail.com>
2024-02-03 14:08:32 +0100

    244bep-42 | worked on ranking
    - enabled vertical scrolling through ranking
    
    - added popup when clicking on an user in ranking

--- ecopoints/app/src/main/java/at/htl/ecopoints/activity/RankingActivity.kt
+++ ecopoints/app/src/main/java/at/htl/ecopoints/activity/RankingActivity.kt
@@ -36,17 +36,23 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.Text
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -60,6 +66,7 @@ import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Popup
 import androidx.compose.ui.zIndex
 import androidx.core.graphics.toColorInt
 import androidx.core.graphics.translationMatrix
@@ -100,6 +107,9 @@ class RankingActivity : ComponentActivity() {
 
     @Composable
     fun DisplayRanking(){
+        var showDetailedRankPopup = remember { mutableStateOf(false) }
+        var selectedUser = remember { mutableStateOf<User?>(null) }
+
         // JUST TESTING-DATA
         val users: Array<User> = arrayOf(
             User(null, "Joe", "123", 547.1),
@@ -109,7 +119,10 @@ class RankingActivity : ComponentActivity() {
             User(null, "Hary", "123", 347.1),
             User(null, "Jane", "123", 333.9),
             User(null, "Max", "123", 313.4),
-            User(null, "Mike", "123", 231.3))
+            User(null, "Mike", "123", 231.3),
+            User(null, "Chloe", "123", 133.9),
+            User(null, "Courtney", "123", 113.4),
+            User(null, "Lisa", "123", 91.3))
 
         val ranks = HashMap<User, Painter>();
         ranks.put(users[0], painterResource(id = R.drawable.ranking_place_1))
@@ -119,14 +132,16 @@ class RankingActivity : ComponentActivity() {
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(0.dp, 150.dp, 0.dp, 0.dp),
+                .padding(0.dp, 170.dp, 0.dp, 0.dp)
+                .verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally
 
         ){
             users.forEach { user ->
                 Button(
                     onClick = {
-
+                        showDetailedRankPopup.value = true
+                        selectedUser.value = user
                     },
                     colors = ButtonDefaults.buttonColors(
                         Color.Transparent
@@ -145,6 +160,15 @@ class RankingActivity : ComponentActivity() {
                                 .height(35.dp)
                         )
                     }
+                    if(ranks.get(user) == null) {
+                        Text(
+                            text = (users.indexOf(user) + 1).toString(),
+                            fontSize = TextUnit(20f, TextUnitType.Sp),
+                            modifier = Modifier
+                                .padding(8.dp)
+                        )
+                    }
+
                     Text(
                         text = user.userName,
                         fontSize = TextUnit(20f, TextUnitType.Sp),
@@ -158,6 +182,37 @@ class RankingActivity : ComponentActivity() {
                 }
             }
         }
+
+        if (showDetailedRankPopup.value) {
+            ShowDetailedRankView(selectedUser.value!!, showDetailedRankPopup)
+        }
+
+    }
+
+    @Composable
+    fun ShowDetailedRankView(user: User, showDetailedRankPopup: MutableState<Boolean>){
+        val onDismiss: () -> Unit = {
+            showDetailedRankPopup.value = false
+        }
+
+        AlertDialog(
+            onDismissRequest = onDismiss,
+            title = {
+                Text(
+                    text = user.userName,
+                    fontSize = TextUnit(25f, TextUnitType.Sp),
+                    fontWeight = androidx.compose.ui.text.font.FontWeight.Bold
+                )
+                    },
+            text = {
+                Text(text = "Eco-Points: " + (user.ecoPoints).toString())
+
+            },
+            confirmButton = {
+                TextButton(onClick = onDismiss) {
+                    Text("Cancel")
+                }
+            })
     }
     
     @Composable

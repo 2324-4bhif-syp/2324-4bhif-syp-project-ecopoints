f1e8bba05b8de6cc6961893f2b23ace47c6eedb1
Armin1503 <arminsalkovic1503@gmail.com>
2024-03-12 10:18:28 +0100

    244bep-42 | added profile-picture and user-name to detail-ranking-view

--- ecopoints/app/src/main/java/at/htl/ecopoints/activity/DetailRankingScreen.kt
+++ ecopoints/app/src/main/java/at/htl/ecopoints/activity/DetailRankingScreen.kt
@@ -13,8 +13,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.ContentAlpha
@@ -34,8 +37,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -44,6 +49,9 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import at.htl.ecopoints.model.User
 import at.htl.ecopoints.R
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 
 
 @Composable
@@ -51,7 +59,7 @@ fun ProfileScreen(user: User, context: Context) {
     val scrollState = rememberScrollState()
 
     Column {
-        BoxWithConstraints(modifier = Modifier.weight(1f)) {
+        BoxWithConstraints() {
             Surface {
                 Column(
                     modifier = Modifier
@@ -63,18 +71,14 @@ fun ProfileScreen(user: User, context: Context) {
                         context.startActivity(intent)
                     }) {
                         androidx.compose.material3.Icon(
+                            modifier = Modifier.size(40.dp),
                             imageVector = Icons.Rounded.ArrowBack,
                             contentDescription = null,
                             tint = androidx.compose.material3.MaterialTheme.colorScheme.primary
                         )
                     }
 
-                    ProfileHeader(
-                        scrollState,
-                        user,
-                        this@BoxWithConstraints.maxHeight
-                    )
-                    ProfileContent(user, this@BoxWithConstraints.maxHeight)
+                    ProfileHeader(user)
                 }
             }
         }
@@ -82,45 +86,21 @@ fun ProfileScreen(user: User, context: Context) {
 }
 
 @Composable
-private fun ProfileHeader(
-    scrollState: ScrollState,
-    user: User,
-    containerHeight: Dp
-) {
-    val offset = (scrollState.value / 2)
-    val offsetDp = with(LocalDensity.current) { offset.toDp() }
-
-    Image(
-        modifier = Modifier
-            .heightIn(max = containerHeight / 2)
-            .fillMaxWidth()
-            .padding(top = offsetDp),
-        painter = painterResource(id = R.drawable.no_profile_pic),
-        contentScale = ContentScale.Crop,
-        contentDescription = null
-    )
-}
-
-@Composable
-private fun ProfileContent(user: User, containerHeight: Dp) {
-    Column {
-        Spacer(modifier = Modifier.height(8.dp))
-
-        Name(user)
-
-        Spacer(Modifier.height((containerHeight - 320.dp).coerceAtLeast(0.dp)))
-    }
-}
-
-@Composable
-private fun Name(
-    user: User
-) {
-    Column(modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 16.dp)) {
+private fun ProfileHeader(user: User) {
+    Row(modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 16.dp)) {
         Text(
+            modifier = Modifier.weight(1f),
             text = user.userName,
-            style = MaterialTheme.typography.h5,
-            fontWeight = FontWeight.Bold
+            fontWeight = FontWeight.Bold,
+            fontSize = TextUnit(30f, TextUnitType.Sp)
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.no_profile_pic),
+            contentDescription = "Profile Picture",
+            modifier = Modifier
+                .size(100.dp)
+                .clip(RoundedCornerShape(80.dp))
         )
     }
 }

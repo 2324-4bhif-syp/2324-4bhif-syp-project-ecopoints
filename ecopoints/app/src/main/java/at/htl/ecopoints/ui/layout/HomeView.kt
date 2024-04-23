e534b16d7b514ddc10b867d9b1abb9e8ae6269a6
ab321 <abdubed2@gmail.com>
2024-04-23 09:18:33 +0200

    244bep-61 | feat: added tankApi and modified home class to migration

--- ecopoints/app/src/main/java/at/htl/ecopoints/ui/layout/HomeView.kt
+++ ecopoints/app/src/main/java/at/htl/ecopoints/ui/layout/HomeView.kt
@@ -1,20 +1,43 @@
 package at.htl.ecopoints.ui.layout
 
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Surface
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color.Companion.DarkGray
+import androidx.compose.ui.graphics.Color.Companion.Gray
+import androidx.compose.ui.graphics.Color.Companion.Green
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import at.htl.ecopoints.R
 import at.htl.ecopoints.model.Store
+import at.htl.ecopoints.model.TankerkoenigApiClient
 import at.htl.ecopoints.navigation.BottomNavBar
 import at.htl.ecopoints.ui.theme.EcoPointsTheme
 import javax.inject.Inject
 import javax.inject.Singleton
+import kotlin.concurrent.thread
 
 @Singleton
 class HomeView {
@@ -30,23 +53,115 @@ class HomeView {
         activity.setContent {
             EcoPointsTheme {
                 Surface(
-                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
                 ) {
-                    Text("Hello Home!")
-                }
 
-                val (currentScreen, setCurrentScreen) = remember { mutableStateOf("Home") }
-                Box(
-                    modifier = Modifier.fillMaxSize()
-                ){
 
-                    BottomNavBar(
-                        currentScreen = currentScreen,
-                        onScreenSelected = { newScreen -> setCurrentScreen(newScreen) },
-                        context = activity
-                    )
+                    ShowPrices()
+
+                    ShowPhoto()
+                    ShowText()
+
+                    //LastTrips()
+
+                    val (currentScreen, setCurrentScreen) = remember { mutableStateOf("Home") }
+                    Box(
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+
+                        BottomNavBar(
+                            currentScreen = currentScreen,
+                            onScreenSelected = { newScreen -> setCurrentScreen(newScreen) },
+                            context = activity
+                        )
+                    }
                 }
             }
         }
     }
+
+
+    @Composable
+    fun ShowPhoto() {
+
+        val painter = painterResource(id = R.drawable.app_icon)
+
+        Box(
+            modifier = Modifier
+        ) {
+            Image(
+                painter = painter,
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.TopStart)
+                    .scale(2.0f)
+                    .padding(top = 30.dp)
+            )
+        }
+    }
+
+    @Composable
+    fun ShowPrices() {
+        var dieselPrice = 0.0;
+        var e5Price = 0.0;
+
+        val tankerkoenigApiClient = TankerkoenigApiClient()
+        try {
+            thread {
+                val gasData = tankerkoenigApiClient.getApiData()
+                dieselPrice = gasData.diesel
+                e5Price = gasData.e5
+            }
+
+            while(dieselPrice == 0.0 && e5Price == 0.0) {
+                Thread.sleep(0.1.toLong())
+            }
+
+        }catch (e: Exception) {
+            Log.e("Tankpreis Error", "Error: ${e.message}")
+        }
+
+        Text(
+            text = buildAnnotatedString {
+                withStyle(style = SpanStyle(fontSize = 25.sp, fontStyle = FontStyle.Italic)) {
+                    append("Diesel\n")
+                }
+                append("  ${dieselPrice}€")
+            },
+            modifier = Modifier.padding(start = 80.dp, top = 150.dp),
+        )
+
+        Text(
+            text = buildAnnotatedString {
+                withStyle(style = SpanStyle(fontSize = 25.sp, fontStyle = FontStyle.Italic)) {
+                    append("Benzin\n")
+                }
+                append("  ${e5Price}€")
+            },
+            modifier = Modifier.padding(start = 260.dp, top = 150.dp),
+        )
+
+    }
+
+    @Composable
+    fun ShowText(){
+        val gradientColors = listOf(Gray, Green, DarkGray)
+
+        Text(
+            text = "Last Rides:",
+            fontSize = 25.sp,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Italic,
+            modifier = Modifier.padding(10.dp, 240.dp, 0.dp,0.dp),
+
+            style = TextStyle(
+                brush = Brush.linearGradient(
+                    colors = gradientColors
+                )
+            )
+        )
+    }
+
 }

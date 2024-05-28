7f65bb37415419c25471d98e4147b256330a78d6
Linus-69420 <Linusnestler@gmail.com>
2024-05-28 11:05:16 +0200

    feat: refactored store

--- ecopoints/app/src/main/java/at/htl/ecopoints/MainActivity.java
+++ ecopoints/app/src/main/java/at/htl/ecopoints/MainActivity.java
@@ -1,51 +1,28 @@
 package at.htl.ecopoints;
 
-
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.util.Log;
-
-import androidx.activity.ComponentActivity;
-import androidx.core.app.ActivityCompat;
 
 import javax.inject.Inject;
 
-import at.htl.ecopoints.model.Store;
-import at.htl.ecopoints.ui.layout.TripView;
-import dagger.hilt.android.AndroidEntryPoint;
+import androidx.activity.ComponentActivity;
+import androidx.compose.ui.platform.ComposeView;
+import at.htl.leonding.MainViewRenderer;import dagger.hilt.android.AndroidEntryPoint;
 
+/** Our main activity implemented in java.
+ * We separate the application logic from the view.
+ * The View is implemented in a separate file (separation of concerns).
+ * We use Kotlin for the Jetpack Compose views.
+ */
 @AndroidEntryPoint
 public class MainActivity extends ComponentActivity {
-    private static final String TAG = MainActivity.class.getSimpleName();
     @Inject
-    TripView tripView;
+    MainViewRenderer mainViewRenderer;
 
-    @Inject
-    Store store;
-
-    @SuppressLint("CheckResult")
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        Log.i(TAG, "onCreate()");
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.BLUETOOTH_CONNECT, Manifest.permission.BLUETOOTH_PRIVILEGED}, 1);
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-                {
-                    return;
-                }
-            }
-        }
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-            }
-
-
         super.onCreate(savedInstanceState);
-        tripView.compose(this);
+        var view = new ComposeView(this);
+        mainViewRenderer.setContent(view);
+        setContentView(view);
     }
 }
--- ecopoints/app/src/main/java/at/htl/ecopoints/model/Store.java
+++ ecopoints/app/src/main/java/at/htl/ecopoints/model/Store.java
@@ -1,32 +1,14 @@
 package at.htl.ecopoints.model;
 
 
-import java.util.function.Consumer;
-
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
-import io.reactivex.rxjava3.subjects.BehaviorSubject;
-
-import javax.inject.Inject;
-import javax.inject.Singleton;
-
-import io.reactivex.rxjava3.subjects.BehaviorSubject;
-
+import at.htl.ecopoints.util.store.StoreBase;
 @Singleton
-public class Store {
-    ModelMapper<Model> mapper;
-    public BehaviorSubject<Model> subject;
-
+public class Store extends StoreBase<Model> {
     @Inject
     public Store() {
-        subject = BehaviorSubject.createDefault(new Model());
-        mapper = new ModelMapper(Model.class);
-    }
-
-    public void next(Consumer<Model> recipe) {
-        var model = mapper.clone(subject.getValue());
-        recipe.accept(model);
-        subject.onNext(model);
+        super(Model.class, new Model());
     }
 }

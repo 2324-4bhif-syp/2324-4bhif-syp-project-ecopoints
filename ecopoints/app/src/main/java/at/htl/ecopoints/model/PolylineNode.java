4185fbd5f15fba9cfb52605ebba13720e001b44d
Linus-69420 <Linusnestler@gmail.com>
2024-10-08 11:24:37 +0200

    feat: migrated everything

---
+++ ecopoints/app/src/main/java/at/htl/ecopoints/model/PolylineNode.java
@@ -0,0 +1,43 @@
+package at.htl.ecopoints.model;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class PolylineNode {
+    private int color;
+    private Double latitude;
+    private Double longitude;
+    private Double fuelCons;
+
+    public PolylineNode() {
+    }
+
+    public PolylineNode(Double latitude, Double longitude, Double fuelCons) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.fuelCons = fuelCons;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Double getFuelCons() {
+        return fuelCons;
+    }
+
+    public void setFuelCons(Double fuelCons) {
+        this.fuelCons = fuelCons;
+    }
+}

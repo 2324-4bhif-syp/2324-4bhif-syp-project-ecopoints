65960558bea7eb9316f6ac5d785be27fc0b9cc8f
Linus-69420 <Linusnestler@gmail.com>
2024-10-21 09:03:33 +0200

    feat: writing of cardata to file is not in the correct json-format

--- ecopoints/app/src/main/java/at/htl/ecopoints/io/JsonFileWriter.java
+++ ecopoints/app/src/main/java/at/htl/ecopoints/io/JsonFileWriter.java
@@ -8,10 +8,13 @@ import dagger.hilt.android.qualifiers.ApplicationContext;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
 
 @Singleton
 public class JsonFileWriter {
@@ -33,13 +36,22 @@ public class JsonFileWriter {
                         throw new RuntimeException("The file could not be created.");
                     }
                 } catch (IOException e) {
-                    Log.e(TAG, "Error creating the file", e);;
+                    Log.e(TAG, "Error creating the file", e);
+                    ;
                     throw new RuntimeException("Error creating the file", e);
                 }
             }
         } else {
             throw new RuntimeException("External storage directory is not available.");
         }
+    }
+
+    public synchronized void startJsonFile() {
+        appendJson("[");
+    }
+
+    public synchronized void endJsonFile() {
+        appendJson("{}]");
     }
 
 
@@ -49,6 +61,15 @@ public class JsonFileWriter {
         } catch (IOException e) {
             Log.e(TAG, "Error writing to the file", e);
             throw new RuntimeException("Error writing to the file", e);
+        }
+    }
+
+    public synchronized String readJson() {
+        try {
+            return Files.readAllLines(file.toPath()).toString();
+        } catch (IOException e) {
+            Log.e(TAG, "Error reading file", e);
+            throw new RuntimeException("Error reading file", e);
         }
     }
 

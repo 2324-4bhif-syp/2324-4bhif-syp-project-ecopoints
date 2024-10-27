35fb1bac391f8c97688103fa660c93a12da5a411
ab321 <abdubed2@gmail.com>
2024-10-27 18:10:39 +0100

    244bep-69 | modified angular project and added important things

--- frontend/ecopoints/src/app/app.component.ts
+++ frontend/ecopoints/src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, importProvidersFrom } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ecopoints';
 }
+
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(HttpClientModule)
+  ]
+}).catch(err => console.error(err));

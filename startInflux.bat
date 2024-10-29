d2dd9a01ba1e63ba2ff1af36483a905eaee2cba7
oliver-420 <oliver.daxinger@gmail.com>
2024-10-29 23:08:16 +0100

    244bep-70 rf: reworked startInflux

--- startInflux.bat
+++ startInflux.bat
@@ -1,17 +1,10 @@
 @echo off
-SET CONTAINER_NAME=influxdb
-SET IMAGE_NAME=influxdb:latest
-SET PORT=8086
 
-REM Check if the container is already running
-docker ps -q -f name=%CONTAINER_NAME% | findstr . >nul
-IF %ERRORLEVEL% EQU 0 (
-    echo Container "%CONTAINER_NAME%" is already running. Starting it...
-    docker start %CONTAINER_NAME%
-) ELSE (
-    echo Container "%CONTAINER_NAME%" does not exist. Pulling the latest image and creating a new container...
-    docker pull %IMAGE_NAME%
-    docker run -d --name %CONTAINER_NAME% -p %PORT%:%PORT% %IMAGE_NAME%
-)
+rem Command to run InfluxDB Docker container
+docker run -d -p 8086:8086 ^
+    -v %CD%\influxdb\data:/var/lib/influxdb ^
+    -v %CD%\influxdb\config:/etc/influxdb ^
+    --name influxdb ^
+    influxdb:latest
 
-echo InfluxDB container is up and running.
+pause

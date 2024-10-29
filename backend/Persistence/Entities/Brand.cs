b2d3bbeb1a1b1f39d3a95b402e4ed00ce9dc2d21
oliver-420 <oliver.daxinger@gmail.com>
2024-10-29 13:42:38 +0100

    244bep-70 ref: removed unused entities

--- backend/Persistence/Entities/Brand.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace Persistence.Entities;
-
-[Table("ECO_BRAND")]
-public class Brand : BaseEntity
-{
-    public string Name { get; set; }
-}
